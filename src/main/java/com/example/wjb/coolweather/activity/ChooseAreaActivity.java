f35303f9895d31a4687c1587c9a48f57488ed1b9
kongkong <244510504@163.com>
2015-11-30 19:39:44 +0800

    完成遍历省市县三级列表的功能

---
+++ src/main/java/com/example/wjb/coolweather/activity/ChooseAreaActivity.java
@@ -0,0 +1,7 @@
+package com.example.wjb.coolweather.activity;
+
+/**
+ * Created by wjb on 2015/11/30.
+ */
+public class ChooseAreaActivity {
+}
---
+++ src/main/java/com/example/wjb/coolweather/util/HttpCallbackListener.java
@@ -0,0 +1,7 @@
+package com.example.wjb.coolweather.util;
+
+/**
+ * Created by wjb on 2015/11/30.
+ */
+public interface HttpCallbackListener {
+}
